36f27da7d6db70620aa3217b8d6d808137790f63
Rohit Yadav <bhaisaab@baagi.org>
2014-01-19 02:29:30 +0530

    fail2ban: add default file and local jail conf
    Signed-off-by: Rohit Yadav <bhaisaab@baagi.org>

--- modules/fail2ban/manifests/init.pp
+++ modules/fail2ban/manifests/init.pp
@@ -3,6 +3,22 @@ class fail2ban {
         ensure => installed
     }
 
+    file { "/etc/default/fail2ban":
+        ensure  => present,
+        owner   => root,
+        group   => root,
+        mode    => 644,
+        source => "puppet:///modules/fail2ban/fail2ban-default",
+    }
+
+    file { "/etc/fail2ban/jail.local":
+        ensure  => present,
+        owner   => root,
+        group   => root,
+        mode    => 644,
+        source => "puppet:///modules/fail2ban/jail.local",
+    }
+
     service { "fail2ban":
         ensure => "running"
     }
