f9126542d906c215a19e0016c286c0cd69a6e966
Rohit Yadav <bhaisaab@baagi.org>
2014-01-19 01:51:56 +0530

    modules: fix quotes in dotfiles
    Signed-off-by: Rohit Yadav <bhaisaab@baagi.org>

--- modules/dotfiles/manifests/init.pp
+++ modules/dotfiles/manifests/init.pp
@@ -6,8 +6,8 @@ class dotfiles($user = 'bhaisaab') {
         path     => "/bin:/usr/bin:/usr/local/bin",
         user     => "$user",
         unless   => "ls /home/${user}/.dotfiles && ls /home/${user}/.oh-my-zsh",
-        command  => 'git clone git://github.com/bhaisaab/dotfiles.git /home/${user}/.dotfiles \
+        command  => "git clone git://github.com/bhaisaab/dotfiles.git /home/${user}/.dotfiles \
                      && git clone git://github.com/robbyrussell/oh-my-zsh.git /home/${user}/.oh-my-zsh \
-                     && /bin/bash /home/${user}/.dotfiles/install.sh',
+                     && /bin/bash /home/${user}/.dotfiles/install.sh",
     }
 }
